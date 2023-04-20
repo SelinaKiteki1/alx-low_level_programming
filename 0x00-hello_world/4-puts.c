26d86663736fd5ea4c61739473c3e8405f7f5439
Selina Kiteki <selinakiteki@gmail.com>
2023-04-20 06:51:48 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -entry
+ * main - entry
  *
  * return: Always 0 (success)
  */
 int main(void)
 {     
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
